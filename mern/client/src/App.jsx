e1c053bf1e411b015628193ca3352417985bd78f
saadhn4 <saadcfi4@gmail.com>
2025-02-07 16:23:57 +0530

    done

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <p style={{ fontWeight: "bold" }}>Hi Saad !</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
